9206b443d081cb74f1ffdb30b5d198c332bd4076
Stefan Stöhr <stefan.stoehr.root@outlook.de>
2019-05-11 18:35:38 +0200

    css change

--- OpenThisGate/open/views.py
+++ OpenThisGate/open/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from script_open import open
+import script_open
 
 def index(request):
     return render(request, 'open/index.html')   
 
 def open(request):
-    open()
+    script_open.open()
     return render(request, 'open/it_is_open.html')       
