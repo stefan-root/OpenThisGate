e2ed906483b2a167d7f9709aa5e1c13267059d85
Pi <pi@pi.de>
2019-05-11 15:28:15 +0000

    Mod views.py

--- OpenThisGate/open/views.py
+++ OpenThisGate/open/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from script_open import open
 
 def index(request):
     return render(request, 'open/index.html')   
 
 def open(request):
+    open()
     return render(request, 'open/it_is_open.html')       
