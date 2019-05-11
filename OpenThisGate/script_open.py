5af48221abaf21e1ffeaa3fabb81cb56af94ede3
Stefan Stöhr <stefan.stoehr.root@outlook.de>
2019-05-11 18:44:03 +0200

    correc indent

--- OpenThisGate/script_open.py
+++ OpenThisGate/script_open.py
@@ -6,21 +6,8 @@ import os
 from time import sleep          # this lets us have a time delay
 
 def open():
-	output_pin = 7
-
-#	GPIO.setboard(GPIO.ZERO)    # Orange Pi PC board
-#	GPIO.setmode(GPIO.BOARD)        # set up BOARD BCM numbering
-#	GPIO.setup(output_pin, GPIO.OUT)         # set BCM7 (pin 26) as an output (LED)
-
         cmd = "./on.sh"
         os.system(cmd)
-
-#	GPIO.output(output_pin, 1)       # set port/pin value to 1/HIGH/True
-	sleep(3)
+        sleep(3)
         cmd = "./off.sh"
         os.system(cmd)
-        
-        #cmd = "echo 0 > /sys/class/gpio/gpio7/value"
-        #os.system(cmd)
-#	GPIO.output(output_pin, 0)       # set port/pin value to 0/LOW/False
-#	GPIO.cleanup()              # Clean GPIO
